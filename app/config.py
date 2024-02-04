8c78e088a5f42a28254e171620a47f04b6fa49d5
Christian Laurean <christianlaurean1@gmail.com>
2024-02-04 10:41:03 -0600

    bugs corregidos

--- app/config.py
+++ app/config.py
@@ -1,9 +1,9 @@
-import json
+import yaml
 from pathlib import Path
 
 def load_config():
     """
-    Loads configuration from the 'config.json' file in the current working directory.
+    Loads configuration from the 'config.yml' file in the current working directory.
 
     Returns:
         dict: Configuration data loaded from the file.
@@ -13,10 +13,10 @@ def load_config():
     current_directory = Path.cwd()
 
     # Construct the path to the config file
-    config_path = current_directory / 'config.json'
+    config_path = current_directory / 'config.yml'
 
     # Open and read the content of the config file
     with open(config_path, mode='r') as file:
-        config_data = json.load(file)
+        config_data = yaml.safe_load(file)
 
     return config_data
--- app/main.py
+++ app/main.py
@@ -1,18 +1,15 @@
 import requests
 import csv
-import os
-import json
 import time
 from datetime import datetime
 from config import load_config
 from pathlib import Path
-from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 
 
 
 
-def fetch_html_content(url:str) -> BeautifulSoup:
+def fetch_html_content(url:str, page:int = None) -> BeautifulSoup:
     """
     Fetches HTML content from the specified URL.
 
@@ -27,19 +24,20 @@ def fetch_html_content(url:str) -> BeautifulSoup:
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
     }
+    if page:
+        # Make the request to the URL
+        response = requests.get(url + str(page), headers=headers)
+    else:
+        response = requests.get(url, headers=headers)
 
-    # Make the request to the URL
-    response = requests.get(url, headers=headers)
     
     # Check for any HTTP errors
     response.raise_for_status()
 
-    # Determine the encoding from the response
-    encoding = response.encoding
     
     # Parse the HTML content using BeautifulSoup
-    return BeautifulSoup(response.text, 'html.parser')
-    
+    return BeautifulSoup(response.text, 'html.parser')    
+
 
 
 
@@ -61,7 +59,7 @@ def extract_links(soup: BeautifulSoup, config: dict):
 
 
 
-def extract_text_from_selector(soup,selector):
+def extract_text_from_selector(soup: BeautifulSoup, selector):
     """
     Extracts text content from the specified selector within a BeautifulSoup object.
 
@@ -76,7 +74,7 @@ def extract_text_from_selector(soup,selector):
     try:
         return soup.select_one(selector).text
     except AttributeError:
-        return None
+        return 'None'
 
 
 
@@ -92,31 +90,46 @@ def parse_links(links,config:dict):
     Yields:
         dict: Product information.
     """
-
+    date = datetime.now()
     for link in links:
+        #Parse the HTML content using BeautifulSoup
         soup = fetch_html_content(link)
-        for indx,html in enumerate(soup.select(config.get('home_product'))):
-            print(f"Extrayendo: {extract_text_from_selector(html,config.get('product_name'))}")
+
+        # load data page in dict
+        for html in soup.select(config.get('home_product')):
+            print(f"extracting: {extract_text_from_selector(html,config.get('product_name'))}")
             yield {
-                    "id" : indx + 1,
                     'product_name' : extract_text_from_selector(html,config.get('product_name')),
-                    'review': extract_text_from_selector(html,config.get('review')),
+                    'review':extract_text_from_selector(html,config.get('review')),
                     'price': extract_text_from_selector(html,config.get('price')),
                     'discount_price': extract_text_from_selector(html,config.get('discount_price')),
-                    'date': str(datetime.now())
+                    'date': date.strftime('%Y/%m/%d')
                     }
 
 
 
-def create_csv(list_data):
-    rute = Path.cwd() / 'data' / 'product_ML.csv'
+
+def create_csv(list_data: list, config: dict):
+    """
+    Creates a CSV file and writes data to it.
+
+    Parameters:
+    - data_list (list): List of dictionaries containing data to be written to the CSV file.
+    - config (dict): Configuration dictionary.
+    """
+
+    # Define the path for the CSV file
+    rute = Path.cwd() / 'data' / config.get('csv_name')
+     # Check if the file already exists
     if not rute.exists():
+        # If not, create a new CSV file and write the header
         with open(rute, mode='a') as file_csv:
             columns = list_data[0].keys()
             archivo = csv.DictWriter(file_csv,fieldnames=columns)
             archivo.writeheader()
             archivo.writerows(list_data)
     else:
+        # If the file already exists, open it and append the data
         with open(rute, mode='a') as file_csv:
             columns = list_data[0].keys()
             archivo = csv.DictWriter(file_csv,fieldnames=columns)
@@ -124,27 +137,39 @@ def create_csv(list_data):
 
         
 
-if __name__ == '__main__':
-    # Load environment variables from a .env file
-    load_dotenv('.env')
-    pagina = 1
-    config = load_config()
-    list_data = []
-    for page in range(1,pagina+1):
-        print("Estrayendo pagina:"+ str(page))
-        URL = f'https://www.mercadolibre.com.mx/ofertas?container_id=MLM779363-1&page={page}'
-        try:
-            soup = fetch_html_content(URL)
-        except Exception as err:
-            print('Error: %s' % err)
 
-        links = extract_links(soup,config)
-        for data in parse_links(links,config):
-            print(data)
-            list_data.append(data)
-        time.sleep(3)
-        print('pagina_descansa')
-    create_csv(list_data)
+if __name__ == '__main__':
+    # Load configuration settings
+    config = load_config()
+    
+    # Get the number of pages to scrape
+    pages = config.get('pages')
+
+    # Initialize an empty list to store scraped data
+    list_data = []
+
+    # Iterate through the provided URLs and pages
+    for url in config.get('urls'):
+        for page in range(1,pages+1):
+            print("extracting page:"+ str(page))
+
+            try:
+                 # Fetch HTML content from the URL
+                soup = fetch_html_content(url,page)
+            except Exception as err:
+                print('Error: %s' % err)
+
+            # Extract links from the HTML content
+            links = extract_links(soup,config)
+
+            # Parse data from the extracted links
+            for data in parse_links(links,config):
+                list_data.append(data)
+        # We wait 5 seconds
+        time.sleep(config.get('time'))
+
+    # Create or append data to the CSV file
+    create_csv(list_data,config)
     
 
 
